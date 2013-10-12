95924424e76f8ec394347421d55d12269c777023
steven <code@yullage.com>
2013-10-12 22:05:52 +0800

    Fix multi-iterations crash bug.

--- Page Rank/src/com/yullage/bigdata2013/PageRank.java
+++ Page Rank/src/com/yullage/bigdata2013/PageRank.java
@@ -25,35 +25,20 @@ public class PageRank {
 	private static final String TEMP_INPUT_PATH = "YullageTempInput";
 
 	public static void main(String[] args) {
-		FileSystem hdfs;
-		JobClient client = new JobClient();
-		JobConf jobConf = new JobConf(PageRank.class);
-
-		// Specify configuration files.
-		// jobConf.addResource(new Path("/opt/hadoop-1.1.2/conf/core-site.xml"));
-		// jobConf.addResource(new Path("/opt/hadoop-1.1.2/conf/hdfs-site.xml"));
-
-		// Specify job name.
-		jobConf.setJobName("Page rank");
-
-		// Specify output types.
-		jobConf.setOutputKeyClass(Text.class);
-		jobConf.setOutputValueClass(Text.class);
-
-		// Specify mapper and reducer.
-		jobConf.setMapperClass(PageRankMapper.class);
-		jobConf.setReducerClass(PageRankReducer.class);
-
-		// Specify input and output format.
-		jobConf.setInputFormat(TextInputFormat.class);
-		jobConf.setOutputFormat(TextOutputFormat.class);
-
+		int iterations = Integer.parseInt(args[0]);
+		
 		// Specify input and output directories.
+		FileSystem hdfs;
 		Path inputPath = new Path(args[1]);
 		Path outputPath = new Path(args[2]);
 		Path tmpInputPath = new Path(TEMP_INPUT_PATH);
-
-		int iterations = Integer.parseInt(args[0]);
+		
+		System.out.println("Iteration 1...");
+		JobClient client = new JobClient();
+		JobConf jobConf = new JobConf(PageRank.class);
+		
+		// First iteration. Use original input path.
+		setJobConf(inputPath, outputPath, jobConf);
 
 		// Remove existing output directory.
 		try {
@@ -64,11 +49,7 @@ public class PageRank {
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-
-		// First iteration. Use original input path.
-		System.out.println("Iteration 1...");
-		FileInputFormat.setInputPaths(jobConf, inputPath);
-		FileOutputFormat.setOutputPath(jobConf, outputPath);
+		
 		client.setConf(jobConf);
 
 		try {
@@ -76,24 +57,31 @@ public class PageRank {
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-
-		// Second and later iterations. Use temporary input directory.
+		
+		// Second and later iterations.
 		for (int i = 1; i < iterations; i++) {
 			System.out.println("Iteration " + (i + 1) + "...");
+			client = new JobClient();
+			jobConf = new JobConf(PageRank.class);
+
+			// Use temporary input directory.
+			setJobConf(tmpInputPath, outputPath, jobConf);
+			
+			// Clean up previous temporary input directory, and
+			// then rename the output directory as temporary
+			// input directory.
 			try {
 				hdfs = tmpInputPath.getFileSystem(jobConf);
 				if (hdfs.exists(tmpInputPath)) {
 					hdfs.delete(tmpInputPath, true);
 				}
-
+				
 				hdfs = outputPath.getFileSystem(jobConf);
 				hdfs.rename(outputPath, tmpInputPath);
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
-
-			FileInputFormat.setInputPaths(jobConf, tmpInputPath);
-			FileOutputFormat.setOutputPath(jobConf, outputPath);
+			
 			client.setConf(jobConf);
 
 			try {
@@ -112,6 +100,31 @@ public class PageRank {
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+	}
+
+	private static void setJobConf(Path inputPath, Path outputPath,
+			JobConf jobConf) {
+		// Specify configuration files.
+		//jobConf.addResource(new Path("/opt/hadoop-1.1.2/conf/core-site.xml"));
+		//jobConf.addResource(new Path("/opt/hadoop-1.1.2/conf/hdfs-site.xml"));
+		
+		// Specify job name.
+		jobConf.setJobName("Page rank");
+
+		// Specify output types.
+		jobConf.setOutputKeyClass(Text.class);
+		jobConf.setOutputValueClass(Text.class);
+
+		// Specify mapper and reducer.
+		jobConf.setMapperClass(PageRankMapper.class);
+		jobConf.setReducerClass(PageRankReducer.class);
+
+		// Specify input and output format.
+		jobConf.setInputFormat(TextInputFormat.class);
+		jobConf.setOutputFormat(TextOutputFormat.class);
+		
+		FileInputFormat.setInputPaths(jobConf, inputPath);
+		FileOutputFormat.setOutputPath(jobConf, outputPath);
 	}
 
 }
