e580023c0368b84b5da5db31f3526f05abb01321
steven <code@yullage.com>
2013-10-12 10:28:38 +0800

    1.0b

--- Number Sort/src/com/yullage/bigdata2013/IntegerSortMapper.java
+++ Number Sort/src/com/yullage/bigdata2013/IntegerSortMapper.java
@@ -11,21 +11,28 @@ import org.apache.hadoop.mapred.Mapper;
 import org.apache.hadoop.mapred.OutputCollector;
 import org.apache.hadoop.mapred.Reporter;
 
-public class NumberSortMapper extends MapReduceBase implements
-		Mapper<LongWritable, Text, IntWritable, IntWritable> {
-	private final static IntWritable one = new IntWritable(1);
+/**
+ * A integer sort mapper implementation.
+ * 
+ * @author Steven Huang
+ * @version 1.0b
+ * 
+ */
+public class IntegerSortMapper extends MapReduceBase implements
+		Mapper<LongWritable, Text, IntWritable, Text> {
+	private final static Text emptyText = new Text("");
 	private IntWritable number = new IntWritable();
 
 	@Override
 	public void map(LongWritable key, Text value,
-			OutputCollector<IntWritable, IntWritable> output, Reporter reporter)
+			OutputCollector<IntWritable, Text> output, Reporter reporter)
 			throws IOException {
 		String line = value.toString();
 		StringTokenizer tokenizer = new StringTokenizer(line);
 
 		while (tokenizer.hasMoreTokens()) {
 			number.set(Integer.parseInt(tokenizer.nextToken()));
-			output.collect(number, one);
+			output.collect(number, emptyText);
 		}
 	}
 
