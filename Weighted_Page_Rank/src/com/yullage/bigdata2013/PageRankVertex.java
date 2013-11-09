22364695d4d0f93271a942480db25d2c94fd84a9
Yu-chun Huang <code@yullage.com>
2013-11-09 22:07:37 +0800

    Add PageRankWritable

--- Weighted_Page_Rank/src/com/yullage/bigdata2013/PageRankVertex.java
+++ Weighted_Page_Rank/src/com/yullage/bigdata2013/PageRankVertex.java
@@ -3,8 +3,6 @@ package com.yullage.bigdata2013;
 import java.io.IOException;
 
 import org.apache.hadoop.conf.Configuration;
-import org.apache.hadoop.io.DoubleWritable;
-import org.apache.hadoop.io.MapWritable;
 import org.apache.hadoop.io.NullWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hama.graph.Vertex;
@@ -12,12 +10,6 @@ import org.apache.hama.graph.Vertex;
 public class PageRankVertex extends Vertex<Text, NullWritable, PageRankWritable> {
 
 	public static double DAMPING_FACTOR = 0.85;
-
-	public static final Text KEY_RANK = new Text("_KEY_RANK");
-	public static final Text KEY_IN_VERTEX = new Text("_KEY_IN_VERTEX");
-	public static final Text KEY_W_IN = new Text("_KEY_W_IN");
-	public static final Text KEY_W_OUT = new Text("_KEY_W_OUT");
-
 	private static int SETUP_STEPS = 2;
 
 	@Override
@@ -35,28 +27,28 @@ public class PageRankVertex extends Vertex<Text, NullWritable, PageRankWritable>
 		// initialize this vertex to 1 / count of global vertices in this
 		// graph
 		if (this.getSuperstepCount() == 0) {
-			PageRankWritable vertexValue = new PageRankWritable();
-			vertexValue.put(KEY_RANK, new DoubleWritable(1.0 / this.getNumVertices()));
-			this.setValue(vertexValue);
+			PageRankWritable prWritable = new PageRankWritable();
+			prWritable.setRank(1.0 / this.getNumVertices());
+			this.setValue(prWritable);
+
 		} else if (this.getSuperstepCount() >= 1) {
 			double sum = 0;
-			for (MapWritable msg : messages) {
-				DoubleWritable rank = new DoubleWritable(Double.parseDouble(msg.get(KEY_RANK).toString()));
-				sum += Double.parseDouble(rank.toString());
+			for (PageRankWritable msg : messages) {
+				sum += msg.getRank().get();
 			}
 
 			double alpha = (1.0d - DAMPING_FACTOR) / this.getNumVertices();
-			this.getValue().put(KEY_RANK, new DoubleWritable(alpha + (sum * DAMPING_FACTOR)));
+			this.getValue().setRank(alpha + (sum * DAMPING_FACTOR));
 		}
 
-		System.out.println("Rank = " + this.getValue().get(KEY_RANK).toString());
+		System.out.println("Rank = " + this.getValue().getRank().toString());
 
 		if (this.getSuperstepCount() < this.getMaxIteration() + SETUP_STEPS) {
 			// Send a new rank to neighbors.
-			Double outRank = Double.parseDouble(this.getValue().get(KEY_RANK).toString()) / this.getEdges().size();
-			PageRankWritable messageContent = new PageRankWritable();
-			messageContent.put(KEY_RANK, new Text(outRank.toString()));
-			sendMessageToNeighbors(messageContent);
+			double outRank = this.getValue().getRank().get() / this.getEdges().size();
+			PageRankWritable msg = new PageRankWritable();
+			msg.setRank(outRank);
+			sendMessageToNeighbors(msg);
 		} else {
 			this.voteToHalt();
 			return;
--- Weighted_Page_Rank/src/com/yullage/bigdata2013/PageRankWritable.java
+++ Weighted_Page_Rank/src/com/yullage/bigdata2013/PageRankWritable.java
@@ -1,10 +1,67 @@
 package com.yullage.bigdata2013;
 
+import org.apache.hadoop.io.DoubleWritable;
 import org.apache.hadoop.io.MapWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.io.Writable;
+import org.apache.hama.bsp.TextArrayWritable;
 
 public class PageRankWritable extends MapWritable {
+	private static final Text KEY_RANK = new Text("_KEY_RANK");
+	private static final Text KEY_IN_VERTEX = new Text("_KEY_IN_VERTEX");
+	private static final Text KEY_WEIGHT = new Text("_KEY_WEIGHT");
+
+	public DoubleWritable getRank() {
+		if (!this.containsKey(KEY_RANK)) {
+			this.put(KEY_RANK, new DoubleWritable());
+		}
+		return (DoubleWritable) this.get(KEY_RANK);
+	}
+
+	public void setRank(DoubleWritable rank) {
+		getRank().set(rank.get());
+	}
+
+	public void setRank(double rank) {
+		getRank().set(rank);
+	}
+
+	public TextArrayWritable getInVertexArray() {
+		if (!this.containsKey(KEY_IN_VERTEX)) {
+			this.put(KEY_IN_VERTEX, new TextArrayWritable());
+		}
+		return (TextArrayWritable) this.get(KEY_IN_VERTEX);
+	}
+
+	public void setInVertexArray(TextArrayWritable inVertexArray) {
+		getInVertexArray().set((Writable[]) inVertexArray.toArray());
+	}
+
+	public MapWritable getWeightMap() {
+		if (!this.containsKey(KEY_WEIGHT)) {
+			this.put(KEY_WEIGHT, new MapWritable());
+		}
+		return (MapWritable) this.get(KEY_WEIGHT);
+	}
+
+	public void setWeightMap(MapWritable weightMap) {
+		getWeightMap().putAll(weightMap);
+	}
+
+	public DoubleWritable getWeight(Text vertexId) {
+		if (getWeightMap().containsKey(vertexId)) {
+			return (DoubleWritable) getWeightMap().get(vertexId);
+		} else {
+			return null;
+		}
+	}
+
+	public void setWeight(Text vertexId, DoubleWritable weight) {
+		getWeightMap().put(vertexId, weight);
+	}
+
 	@Override
 	public String toString() {
-		return this.get(PageRankVertex.KEY_RANK).toString();
+		return getRank().toString();
 	}
 }
