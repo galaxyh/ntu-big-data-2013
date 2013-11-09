cdae773dbfa911abf66d02906ac795699c7ea432
Yu-chun Huang <code@yullage.com>
2013-11-09 10:00:22 +0800

    test

--- Weighted_Page_Rank/src/com/yullage/bigdata2013/PageRankVertex.java
+++ Weighted_Page_Rank/src/com/yullage/bigdata2013/PageRankVertex.java
@@ -24,11 +24,10 @@ public class PageRankVertex extends Vertex<Text, NullWritable, DoubleWritable> {
 	public void compute(Iterable<DoubleWritable> messages) throws IOException {
 		// initialize this vertex to 1 / count of global vertices in this
 		// graph
-		System.out.println("Vertex " + this.getVertexID() + "ST count="
+		System.out.println("Vertex " + this.getVertexID() + " ST count="
 				+ this.getSuperstepCount());
-		if (this.getSuperstepCount() == 5) {
-			this.voteToHalt();
-		} else if (this.getSuperstepCount() == 0) {
+
+		if (this.getSuperstepCount() == 0) {
 			this.setValue(new DoubleWritable(1.0 / this.getNumVertices()));
 		} else if (this.getSuperstepCount() >= 1) {
 			double sum = 0;
@@ -39,6 +38,11 @@ public class PageRankVertex extends Vertex<Text, NullWritable, DoubleWritable> {
 			this.setValue(new DoubleWritable(alpha + (sum * DAMPING_FACTOR)));
 		}
 
+		if (this.getSuperstepCount() == 5) {
+			this.voteToHalt();
+			return;
+		}
+		
 		// in each superstep we are going to send a new rank to our
 		// neighbours
 		sendMessageToNeighbors(new DoubleWritable(this.getValue().get()
