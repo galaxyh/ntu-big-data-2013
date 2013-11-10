c11c988c90b501e171321037e30a1d90c4573131
Yu-chun Huang <code@yullage.com>
2013-11-10 09:43:26 +0800

    In and out edge counts.

--- Weighted_Page_Rank/src/com/yullage/bigdata2013/PageRankVertex.java
+++ Weighted_Page_Rank/src/com/yullage/bigdata2013/PageRankVertex.java
@@ -1,6 +1,8 @@
 package com.yullage.bigdata2013;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.io.NullWritable;
@@ -10,8 +12,13 @@ import org.apache.hama.graph.Vertex;
 public class PageRankVertex extends Vertex<Text, NullWritable, PageRankWritable> {
 
 	public static double DAMPING_FACTOR = 0.85;
-	private static int SETUP_STEPS = 2;
+	private static int SETUP_STEPS = 3;
 
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.apache.hama.graph.Vertex#setup(org.apache.hadoop.conf.Configuration)
+	 */
 	@Override
 	public void setup(Configuration conf) {
 		String val = conf.get("hama.pagerank.alpha");
@@ -20,18 +27,35 @@ public class PageRankVertex extends Vertex<Text, NullWritable, PageRankWritable>
 		}
 	}
 
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.apache.hama.graph.VertexInterface#compute(java.lang.Iterable)
+	 */
 	@Override
 	public void compute(Iterable<PageRankWritable> messages) throws IOException {
 		System.out.println("Vertex = " + this.getVertexID() + " Superstep = " + this.getSuperstepCount());
 
-		// initialize this vertex to 1 / count of global vertices in this
-		// graph
 		if (this.getSuperstepCount() == 0) {
-			PageRankWritable prWritable = new PageRankWritable();
-			prWritable.setRank(1.0 / this.getNumVertices());
-			this.setValue(prWritable);
+			// initialize this vertex to 1/count of global vertices in this graph.
+			PageRankWritable vertexContent = new PageRankWritable();
+			vertexContent.setRank(1.0 / this.getNumVertices());
+			this.setValue(vertexContent);
 
-		} else if (this.getSuperstepCount() >= 1) {
+			// Broadcast this vertex ID for neighbors to calculate in and out edge counts.
+			broadcastVertexId();
+
+		} else if (this.getSuperstepCount() == 1) {
+			// Calculate in and out edge counts. Then send these information back to senders.
+			sendInOutEdgeCounts(messages);
+		} else if (this.getSuperstepCount() == 2) {
+			// Calculate weight for each neighbor.
+			calculateWeight(messages);
+
+			// Just continue to next super step.
+			return;
+
+		} else if (this.getSuperstepCount() >= 3) {
 			double sum = 0;
 			for (PageRankWritable msg : messages) {
 				sum += msg.getRank().get();
@@ -44,15 +68,62 @@ public class PageRankVertex extends Vertex<Text, NullWritable, PageRankWritable>
 		System.out.println("Rank = " + this.getValue().getRank().toString());
 
 		if (this.getSuperstepCount() < this.getMaxIteration() + SETUP_STEPS) {
-			// Send a new rank to neighbors.
-			double outRank = this.getValue().getRank().get() / this.getEdges().size();
-			PageRankWritable msg = new PageRankWritable();
-			msg.setRank(outRank);
-			sendMessageToNeighbors(msg);
+			sendNewRank();
 		} else {
 			this.voteToHalt();
 			return;
 		}
+	}
+
+	/**
+	 * Broadcast this vertex ID for neighbors to calculate in and out edge counts.
+	 * 
+	 * @throws IOException
+	 */
+	private void broadcastVertexId() throws IOException {
+		PageRankWritable msg = new PageRankWritable();
+		msg.setSenderId(this.getVertexID());
+		sendMessageToNeighbors(msg);
+	}
+
+	/**
+	 * Calculate in and out edge counts. Send these information back to senders.
+	 * 
+	 * @param messages
+	 * @throws IOException
+	 */
+	private void sendInOutEdgeCounts(Iterable<PageRankWritable> messages) throws IOException {
+		// Receive vertex IDs from all sender.
+		List<Text> vertexIdList = new ArrayList<Text>();
+		for (PageRankWritable msg : messages) {
+			vertexIdList.add(msg.getSenderId());
+		}
+
+		// Send incoming and outgoing edge counts back to senders.
+		PageRankWritable msg = new PageRankWritable();
+		msg.setSenderId(getVertexID());
+		msg.setInEdgeCount(vertexIdList.size());
+		msg.setOutEdgeCount(getEdges().size());
+
+		for (Text id : vertexIdList) {
+			sendMessage(id, msg);
+		}
+	}
+
+	private void calculateWeight(Iterable<PageRankWritable> messages) {
+
+	}
+
+	/**
+	 * Send new rank to neighbors.
+	 * 
+	 * @throws IOException
+	 */
+	private void sendNewRank() throws IOException {
+		double outRank = this.getValue().getRank().get() / getEdges().size();
+		PageRankWritable msg = new PageRankWritable();
+		msg.setRank(outRank);
+		sendMessageToNeighbors(msg);
 	}
 
 }
--- Weighted_Page_Rank/src/com/yullage/bigdata2013/PageRankWritable.java
+++ Weighted_Page_Rank/src/com/yullage/bigdata2013/PageRankWritable.java
@@ -1,16 +1,24 @@
 package com.yullage.bigdata2013;
 
 import org.apache.hadoop.io.DoubleWritable;
+import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.MapWritable;
 import org.apache.hadoop.io.Text;
-import org.apache.hadoop.io.Writable;
-import org.apache.hama.bsp.TextArrayWritable;
 
+/**
+ * @author Yu-chun Huang
+ * 
+ */
 public class PageRankWritable extends MapWritable {
-	private static final Text KEY_RANK = new Text("_KEY_RANK");
-	private static final Text KEY_IN_VERTEX = new Text("_KEY_IN_VERTEX");
-	private static final Text KEY_WEIGHT = new Text("_KEY_WEIGHT");
+	private static final Text KEY_RANK = new Text("__KEY_RANK");
+	private static final Text KEY_SENDER_VERTEX_ID = new Text("__KEY_SENDER_VERTEX_ID");
+	private static final Text KEY_IN_EDGE_COUNT = new Text("__KEY_IN_VERTEX_COUNT");
+	private static final Text KEY_OUT_EDGE_COUNT = new Text("__KEY_OUT_VERTEX_COUNT");
+	private static final Text KEY_WEIGHT = new Text("__KEY_WEIGHT");
 
+	/**
+	 * @return Rank
+	 */
 	public DoubleWritable getRank() {
 		if (!this.containsKey(KEY_RANK)) {
 			this.put(KEY_RANK, new DoubleWritable());
@@ -18,25 +26,88 @@ public class PageRankWritable extends MapWritable {
 		return (DoubleWritable) this.get(KEY_RANK);
 	}
 
+	/**
+	 * @param rank
+	 */
 	public void setRank(DoubleWritable rank) {
 		getRank().set(rank.get());
 	}
 
+	/**
+	 * @param rank
+	 */
 	public void setRank(double rank) {
 		getRank().set(rank);
 	}
 
-	public TextArrayWritable getInVertexArray() {
-		if (!this.containsKey(KEY_IN_VERTEX)) {
-			this.put(KEY_IN_VERTEX, new TextArrayWritable());
+	/**
+	 * @return Sender vertex ID
+	 */
+	public Text getSenderId() {
+		if (!this.containsKey(KEY_SENDER_VERTEX_ID)) {
+			this.put(KEY_SENDER_VERTEX_ID, new Text());
 		}
-		return (TextArrayWritable) this.get(KEY_IN_VERTEX);
+		return (Text) this.get(KEY_SENDER_VERTEX_ID);
 	}
 
-	public void setInVertexArray(TextArrayWritable inVertexArray) {
-		getInVertexArray().set((Writable[]) inVertexArray.toArray());
+	/**
+	 * @param vertexId
+	 */
+	public void setSenderId(Text vertexId) {
+		getSenderId().set(vertexId);
 	}
 
+	/**
+	 * @return Incoming edge count
+	 */
+	public LongWritable getInEdgeCount() {
+		if (!this.containsKey(KEY_IN_EDGE_COUNT)) {
+			this.put(KEY_IN_EDGE_COUNT, new LongWritable());
+		}
+		return (LongWritable) this.get(KEY_IN_EDGE_COUNT);
+	}
+
+	/**
+	 * @param inEdgeCount
+	 */
+	public void setInEdgeCount(LongWritable inEdgeCount) {
+		getOutEdgeCount().set(inEdgeCount.get());
+	}
+
+	/**
+	 * @param inEdgeCount
+	 */
+	public void setInEdgeCount(long inEdgeCount) {
+		getOutEdgeCount().set(inEdgeCount);
+	}
+
+	/**
+	 * @return Outgoing edge count
+	 */
+	public LongWritable getOutEdgeCount() {
+		if (!this.containsKey(KEY_OUT_EDGE_COUNT)) {
+			this.put(KEY_OUT_EDGE_COUNT, new LongWritable());
+		}
+		return (LongWritable) this.get(KEY_OUT_EDGE_COUNT);
+	}
+
+	/**
+	 * @param outEdgeCount
+	 */
+	public void setOutEdgeCount(LongWritable outEdgeCount) {
+		getOutEdgeCount().set(outEdgeCount.get());
+	}
+
+	/**
+	 * @param outEdgeCount
+	 */
+	public void setOutEdgeCount(long outEdgeCount) {
+		getOutEdgeCount().set(outEdgeCount);
+	}
+
+	/**
+	 * @return Map of weight values for each neighbor
+	 */
 	public MapWritable getWeightMap() {
 		if (!this.containsKey(KEY_WEIGHT)) {
 			this.put(KEY_WEIGHT, new MapWritable());
@@ -44,10 +115,17 @@ public class PageRankWritable extends MapWritable {
 		return (MapWritable) this.get(KEY_WEIGHT);
 	}
 
+	/**
+	 * @param weightMap
+	 */
 	public void setWeightMap(MapWritable weightMap) {
 		getWeightMap().putAll(weightMap);
 	}
 
+	/**
+	 * @param vertexId
+	 * @return Weight values of the specified neighbor
+	 */
 	public DoubleWritable getWeight(Text vertexId) {
 		if (getWeightMap().containsKey(vertexId)) {
 			return (DoubleWritable) getWeightMap().get(vertexId);
@@ -56,10 +134,19 @@ public class PageRankWritable extends MapWritable {
 		}
 	}
 
+	/**
+	 * @param vertexId
+	 * @param weight
+	 */
 	public void setWeight(Text vertexId, DoubleWritable weight) {
 		getWeightMap().put(vertexId, weight);
 	}
 
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see java.lang.Object#toString()
+	 */
 	@Override
 	public String toString() {
 		return getRank().toString();
